0587fab8ecb823b82a56b6044d923881c73bb59f
tmkkmac@gmail.com <tmkkmac@gmail.com@06cd4574-f03a-dd4f-40e6-7570b67c5ae5>
2014-11-08 08:54:35 +0000

    DSDDecoder: profile support
    git-svn-id: https://svn.code.sf.net/p/xld/code/trunk@306 06cd4574-f03a-dd4f-40e6-7570b67c5ae5

---
+++ XLDDSDDecoder/XLDDSDDecoderConfig.h
@@ -0,0 +1,22 @@
+//
+//  XLDDSDDecoderConfig.h
+//  XLDDSDDecoder
+//
+//  Created by tmkk on 14/05/12.
+//  Copyright 2014 tmkk. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface XLDDSDDecoderConfig : NSObject {
+	IBOutlet id o_prefPane;
+	IBOutlet id o_samplerate;
+	IBOutlet id o_srcAlgorithm;
+	IBOutlet id o_gain;
+	IBOutlet id o_text1;
+}
+
+- (IBAction)statusChanged:(id)sender;
+
+@end
---
+++ XLDDSDDecoder/XLDDSDDecoderConfig.m
@@ -0,0 +1,66 @@
+//
+//  XLDDSDDecoderConfig.m
+//  XLDDSDDecoder
+//
+//  Created by tmkk on 14/05/12.
+//  Copyright 2014 tmkk. All rights reserved.
+//
+
+#import "XLDDSDDecoderConfig.h"
+
+
+@implementation XLDDSDDecoderConfig
+
+- (id)init
+{
+	[super init];
+	NSDictionary *defaultValues = [NSDictionary dictionaryWithObjectsAndKeys:
+								   [NSNumber numberWithInt:0], @"XLDDSDDecoderSamplerate",
+								   [NSNumber numberWithInt:4], @"XLDDSDDecoderSRCAlgorithm",
+								   [NSNumber numberWithInt:0], @"XLDDSDDecoderQuantization",
+								   [NSNumber numberWithInt:0], @"XLDDSDDecoderGain",nil];
+	[[NSUserDefaultsController sharedUserDefaultsController] setInitialValues:defaultValues];
+	[NSBundle loadNibNamed:@"XLDDSDDecoder" owner:self];
+	[self statusChanged:nil];
+	return self;
+}
+
+- (void)prefPane
+{
+	[o_prefPane makeKeyAndOrderFront:self];
+}
+
+- (IBAction)statusChanged:(id)sender
+{
+	if([[o_samplerate selectedItem] tag] > 0) {
+		[o_srcAlgorithm setEnabled:YES];
+		[o_text1 setTextColor:[NSColor blackColor]];
+	}
+	else {
+		[o_srcAlgorithm setEnabled:NO];
+		[o_text1 setTextColor:[NSColor lightGrayColor]];
+	}
+}
+
+- (NSDictionary *)configurations
+{
+	NSUserDefaults *pref = [NSUserDefaults standardUserDefaults];
+	NSDictionary *dict = [NSDictionary dictionaryWithObjectsAndKeys:
+								   [pref objectForKey:@"XLDDSDDecoderSamplerate"], @"XLDDSDDecoderSamplerate",
+								   [pref objectForKey:@"XLDDSDDecoderSRCAlgorithm"], @"XLDDSDDecoderSRCAlgorithm",
+								   [pref objectForKey:@"XLDDSDDecoderQuantization"], @"XLDDSDDecoderQuantization",
+								   [pref objectForKey:@"XLDDSDDecoderGain"], @"XLDDSDDecoderGain",nil];
+	return dict;
+}
+
+- (void)loadConfigurations:(id)dict
+{
+	NSUserDefaults *pref = [NSUserDefaults standardUserDefaults];
+	[pref setObject:[dict objectForKey:@"XLDDSDDecoderSamplerate"] forKey:@"XLDDSDDecoderSamplerate"];
+	[pref setObject:[dict objectForKey:@"XLDDSDDecoderSRCAlgorithm"] forKey:@"XLDDSDDecoderSRCAlgorithm"];
+	[pref setObject:[dict objectForKey:@"XLDDSDDecoderQuantization"] forKey:@"XLDDSDDecoderQuantization"];
+	[pref setObject:[dict objectForKey:@"XLDDSDDecoderGain"] forKey:@"XLDDSDDecoderGain"];
+	[pref synchronize];
+}
+
+@end
